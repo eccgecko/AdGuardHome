a0cba3856caceb2c391c916a71f9544232d3c6fb
eccgecko <36884529+eccgecko@users.noreply.github.com>
2023-02-22 18:12:15 +0000

    Update AdGuardHomeUpdate_linux_arm64.sh

--- scripts/AdGuardHomeUpdate_linux_arm64.sh
+++ scripts/AdGuardHomeUpdate_linux_arm64.sh
@@ -16,7 +16,7 @@ fi
 TARBALL="AdGuardHome_linux_arm64.tar.gz"
 DATE=$(date +%Y.%m.%d.%H:%M)
 WORKING_DIR="$HOME/my-agh-update"
-LOG_DIR="$WORKING_DIR"
+LOG="$WORKING_DIR/$DATE-update.log"
 BACKUP_DIR="$WORKING_DIR/$DATE"
 LATEST_RELEASE=$(curl -s https://api.github.com/repos/AdguardTeam/AdGuardHome/releases/latest | grep tag_name | cut -d '"' -f 4) 
 DOWNLOAD_URL="https://github.com/AdguardTeam/AdGuardHome/releases/download/$LATEST_RELEASE/$TARBALL"
@@ -34,7 +34,7 @@ fi
 
 # Download the latest release of AdGuardHome
 echo "Downloading latest AdGuardHome tarball..."
-if ! wget -q -O "/tmp/$TARBALL" "$DOWNLOAD_URL" 2>> "$LOG_DIR/$DATE-update.log" ; then
+if ! wget -q -O "/tmp/$TARBALL" "$DOWNLOAD_URL" 2>> "$LOG" ; then
 	echo "Error: Failed to download AdGuardHome tarball." >&2
 	exit 1
 fi
@@ -42,7 +42,7 @@ fi
 
 # Download the checksum file and verify the sha256 checksum of the downloaded tarball
 echo "Verifying checksum..."
-if ! wget -q -O "/tmp/checksums.txt" "$CHECKSUM_URL" 2>> "$LOG_DIR/adguard-update-$DATE.log" ; then
+if ! wget -q -O "/tmp/checksums.txt" "$CHECKSUM_URL" 2>> "$LOG" ; then
 	echo "Error: Failed to download checksum file." >&2
 	exit 1
 fi
@@ -59,18 +59,18 @@ fi
 # Stop the current running AdGuardHome service and create a backup of the AdGuardHome configuration and data
 echo "Stopping AdGuardHome service and creating backup..."
 if $SUDO systemctl is-active --quiet AdGuardHome.service; then
-	$SUDO systemctl stop AdGuardHome.service 2>> "$LOG_DIR/adguard-update-$DATE.log" || { echo "Error: Failed to stop AdGuardHome service." >&2; exit 1; }
+	$SUDO systemctl stop AdGuardHome.service 2>> "$LOG" || { echo "Error: Failed to stop AdGuardHome service." >&2; exit 1; }
 	else echo "AdGuardHome service is not running."
 fi
 
 
 
-if ! $SUDO cp /opt/AdGuardHome/AdGuardHome.yaml "$BACKUP_DIR" 2>> "$LOG_DIR/adguard-update-$DATE.log" ; then
+if ! $SUDO cp /opt/AdGuardHome/AdGuardHome.yaml "$BACKUP_DIR" 2>> "$LOG" ; then
 	echo "Error: Failed to create backup of AdGuardHome configuration file." >&2
 	exit 1
 fi
 
-if ! $SUDO cp -r /opt/AdGuardHome/data "$BACKUP_DIR" 2>> "$LOG_DIR/adguard-update-$DATE.log" ; then
+if ! $SUDO cp -r /opt/AdGuardHome/data "$BACKUP_DIR" 2>> "$LOG" ; then
 	echo "Error: Failed to create backup of AdGuardHome data directory." >&2
 	exit 1
 fi
@@ -78,19 +78,19 @@ fi
 
 # Extract the downloaded tarball and copy the new AdGuardHome binary to /opt/AdGuardHome
 echo "Extracting into /tmp/ and and installing updated AdGuardHome binary..."
-if ! tar -zxvf "/tmp/$TARBALL" -C /tmp/ 2>> "$LOG_DIR/adguard-update-$DATE.log" ; then
+if ! tar -zxvf "/tmp/$TARBALL" -C /tmp/ 2>> "$LOG" ; then
 	echo "Error: Failed to extract AdGuardHome tarball." >&2
 	exit 1
 fi
 
-if ! $SUDO cp /tmp/AdGuardHome/AdGuardHome /opt/AdGuardHome/ 2>> "$LOG_DIR/adguard-update-$DATE.log" ; then
+if ! $SUDO cp /tmp/AdGuardHome/AdGuardHome /opt/AdGuardHome/ 2>> "$LOG" ; then
 	echo "Error: Failed to copy new AdGuardHome binary to /opt/AdGuardHome/." >&2
 	exit 1
 fi
 
 # Start the AdGuardHome service
 echo "Starting the AdGuardHome service..."
-if ! $SUDO systemctl start AdGuardHome.service 2>> "$LOG_DIR/adguard-update-$DATE.log" ; then
+if ! $SUDO systemctl start AdGuardHome.service 2>> "$LOG" ; then
 	echo "Error: Failed to start AdGuardHome service." >&2
 	exit 1
 		else sleep 5 # Wait for service to start up
